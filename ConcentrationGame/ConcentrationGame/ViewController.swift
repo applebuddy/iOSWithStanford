859ea7e871c93cb56173410962d5b507b6c089d2
MinKyeongTae <kyeongtea92@gmail.com>
2019-08-16 22:45:27 +0900

    First Commit with Stanford iOS Study

---
+++ ConcentrationGame/ConcentrationGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by MinKyeongTae on 16/08/2019.
+//  Copyright © 2019 MinKyeongTae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
